192cc63c3286c4e126d8c2a56cae212e87a57913
khanmqa <khan.m.qa@gmail.com>
2019-06-27 13:35:18 +0100

    search feature step definition
    creating ruby automation steps to execute scenario

---
+++ capybara/features/step_definitions/search_steps.rb
@@ -0,0 +1,18 @@
+Given("I am on SpringerNature website") do
+  visit('http://link.springer.com')
+end
+
+When("I search for HIIT") do
+fill_in('query', :with => 'HIIT')
+click_button('search')
+sleep(10)
+end
+
+Then("I able to see relevant results") do
+within(:css,"h1#number-of-search-results-and-search-terms")do
+page.should have_content('Result(s) for HIIT')
+end
+end
+
+
+
