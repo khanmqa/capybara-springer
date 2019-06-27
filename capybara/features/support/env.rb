9d5d7ff0e9c06ee8432ae4205a582b2ec873cdd1
khanmqa <khan.m.qa@gmail.com>
2019-06-27 13:34:17 +0100

    setting up capybara framework
    setting up the framework to work with chrome

---
+++ capybara/features/support/env.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'capybara'
+require 'capybara/dsl'
+require 'rspec'
+ 
+Capybara.run_server = false
+Capybara.default_driver = :selenium_chrome
+Capybara.default_selector = :css
+ 
+module Helpers
+  def without_resynchronize
+    page.driver.options[:resynchronize] = false
+    yield
+    page.driver.options[:resynchronize] = true
+  end
+end
+World(Capybara::DSL, Helpers)
